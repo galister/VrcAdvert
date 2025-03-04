db612ffbd93297101462d5684a131d3a4b19755a
galister <22305755+galister@users.noreply.github.com>
2025-03-05 07:47:48 +0900

    ports are ushort

--- Program.cs
+++ Program.cs
@@ -7,13 +7,25 @@ using VRC.OSCQuery;
 var rootCommand = new RootCommand
 {
     Name = "VrcAdvert",
-    Description = "Advertise your OSC app through OSCQuery."
+    Description = "Advertise your OSC app through OSCQuery.",
 };
 
-var nameArg = new Argument<string>("name", "The name of your OSC app. Can be anything as long as it's unique");
-var httpPortArg = new Argument<short>("http_port", "The port to use for OSCQuery. Can be any free TCP port.");
-var oscPortArg = new Argument<short>("osc_port", "The port that your OSC app is listening on. VRC will send OSC avatar parameters to this port.");
-var trackingOpt = new Option<bool>("--tracking", "Add this if you want to receive /tracking/vrsystem messages.");
+var nameArg = new Argument<string>(
+    "name",
+    "The name of your OSC app. Can be anything as long as it's unique"
+);
+var httpPortArg = new Argument<ushort>(
+    "http_port",
+    "The port to use for OSCQuery. Can be any free TCP port."
+);
+var oscPortArg = new Argument<ushort>(
+    "osc_port",
+    "The port that your OSC app is listening on. VRC will send OSC avatar parameters to this port."
+);
+var trackingOpt = new Option<bool>(
+    "--tracking",
+    "Add this if you want to receive /tracking/vrsystem messages."
+);
 
 rootCommand.AddArgument(nameArg);
 rootCommand.AddArgument(httpPortArg);
@@ -22,7 +34,7 @@ rootCommand.AddOption(trackingOpt);
 rootCommand.SetHandler(ctx =>
 {
     var pr = ctx.ParseResult;
-    
+
     var oscQuery = new OSCQueryServiceBuilder()
         .WithServiceName(pr.GetValueForArgument(nameArg))
         .WithHostIP(IPAddress.Loopback)
@@ -33,17 +45,31 @@ rootCommand.SetHandler(ctx =>
         .AdvertiseOSC()
         .AdvertiseOSCQuery()
         .Build();
-    
-    Console.WriteLine($"{oscQuery.ServerName} running at {oscQuery.HostIP} tcp:{oscQuery.TcpPort} osc:{oscQuery.OscPort}");
+
+    Console.WriteLine(
+        $"{oscQuery.ServerName} running at {oscQuery.HostIP} tcp:{oscQuery.TcpPort} osc:{oscQuery.OscPort}"
+    );
 
     oscQuery.RefreshServices();
-    oscQuery.AddEndpoint("/avatar/parameters/VSync", "b", Attributes.AccessValues.WriteOnly);
+    oscQuery.AddEndpoint("/avatar/parameters", "b", Attributes.AccessValues.WriteOnly);
 
     if (pr.GetValueForOption(trackingOpt))
     {
-        oscQuery.AddEndpoint("/tracking/vrsystem/head/pose", "ffffff", Attributes.AccessValues.WriteOnly);
-        oscQuery.AddEndpoint("/tracking/vrsystem/leftwrist/pose", "ffffff", Attributes.AccessValues.WriteOnly);
-        oscQuery.AddEndpoint("/tracking/vrsystem/rightwrist/pose", "ffffff", Attributes.AccessValues.WriteOnly);
+        oscQuery.AddEndpoint(
+            "/tracking/vrsystem/head/pose",
+            "ffffff",
+            Attributes.AccessValues.WriteOnly
+        );
+        oscQuery.AddEndpoint(
+            "/tracking/vrsystem/leftwrist/pose",
+            "ffffff",
+            Attributes.AccessValues.WriteOnly
+        );
+        oscQuery.AddEndpoint(
+            "/tracking/vrsystem/rightwrist/pose",
+            "ffffff",
+            Attributes.AccessValues.WriteOnly
+        );
     }
 
     var knownServices = new HashSet<string>();
@@ -51,8 +77,10 @@ rootCommand.SetHandler(ctx =>
     {
         if (knownServices.Contains(profile.name))
             return;
-    
-        Console.WriteLine($"Found service {profile.name} at {profile.address}:{profile.port} with type {profile.serviceType}");
+
+        Console.WriteLine(
+            $"Found service {profile.name} at {profile.address}:{profile.port} with type {profile.serviceType}"
+        );
         knownServices.Add(profile.name);
     }
 
@@ -60,8 +88,9 @@ rootCommand.SetHandler(ctx =>
     oscQuery.OnOscServiceAdded += LogDiscovery;
 
     Console.WriteLine("Ready.");
-    
+
     Thread.Sleep(int.MaxValue);
 });
 
 return await rootCommand.InvokeAsync(args);
+
